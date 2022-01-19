5f9fe39b8f1a0e23ef70b38586c46fcf134144db
Yazan Deek <ydeek91@yahoo.com>
2022-01-19 12:57:54 -0700

    displayed menu with WP function

--- themes/gymfitnessWP/header.php
+++ themes/gymfitnessWP/header.php
@@ -21,7 +21,21 @@
                 </div>
                 <!--logo!-->
 
-                <!-- TODO: add menu -->
+                <?php
+
+                $args = array(
+                    'theme_location' => 'main-menu',
+                    'container' => 'nav',
+                    'container_class' => 'main-menu' // to target with css
+
+                );
+
+                wp_nav_menu($args);
+
+
+
+                ?>
+
 
 
             </div>
--- themes/gymfitnessWP/index.php
+++ themes/gymfitnessWP/index.php
@@ -1,2 +1,6 @@
+<?php get_header(); ?>
+
+
 <?php
 echo "Hello World";
+?>
