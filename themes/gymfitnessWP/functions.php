31c5c15cb368e181da097795d1768a474af5cf60
Yazan Deek <ydeek91@yahoo.com>
2022-01-19 13:24:17 -0700

    linked normalize.css file and added google fonts

--- themes/gymfitnessWP/functions.php
+++ themes/gymfitnessWP/functions.php
@@ -16,3 +16,22 @@ function gymfitness_menus()
 //Hook
 
 add_action('init', 'gymfitness_menus');
+
+
+// Add Stylesheets and JS files 
+
+function gymfitness_scripts()
+{
+
+    //Normalize CSS
+
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+    // Google Font 
+
+    wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
+
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+}
+
+add_action('wp_enqueue_scripts', 'gymfitness_scripts');
