577b2b230c7451bf262ecce8d5ec9c180232d4be
Yazan Deek <ydeek91@yahoo.com>
2022-01-19 09:57:52 -0700

    created divs and their classes ifor navigation bar section in header.php

---
+++ themes/gymfitnessWP/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+
+    <header class="site-header">
+        <div class="container header-grid">
+            <div class="navigation-bar">
+                <div class="logo">
+                    <a href="<?php echo home_url(); ?>">
+                        <img src="<?php echo get_template_directory_uri() . "/img/logo.svg"; ?>" alt="site logo">
+
+                    </a>
+                </div>
+                <!--logo!-->
+
+                <!-- TODO: add menu -->
+
+
+            </div>
+            <!--navigation-br-->
+
+        </div>
+        <!--ccontainer-->
+
+
+    </header>
