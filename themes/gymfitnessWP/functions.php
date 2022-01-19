d8e9f913e001d7bd453c54aca015b1aaadf05fe0
Yazan Deek <ydeek91@yahoo.com>
2022-01-19 13:30:28 -0700

    link main stylesheet using WP function

--- themes/gymfitnessWP/functions.php
+++ themes/gymfitnessWP/functions.php
@@ -31,7 +31,10 @@ function gymfitness_scripts()
 
     wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
 
-    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+    // MAin Stylesheet 
+
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googlefont'), '1.0.0');
 }
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
