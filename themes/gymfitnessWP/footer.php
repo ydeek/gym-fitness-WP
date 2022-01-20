6c9a0f20432e87d5a92a9a77286220288e84fcd1
Yazan Deek <ydeek91@yahoo.com>
2022-01-20 04:25:39 -0700

    started coding the footer

--- themes/gymfitnessWP/footer.php
+++ themes/gymfitnessWP/footer.php
@@ -1,3 +1,28 @@
+<footer class="site-footer" container>
+    <div class="footer-content">
+        <?php
+
+        $args = array(
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'container_class' => 'main-menu' // to target with css
+
+        );
+
+        wp_nav_menu($args);
+
+
+
+        ?>
+
+    </div>
+</footer>
+
+
+
+
+
+
 </body>
 
 </html>
